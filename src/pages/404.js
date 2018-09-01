209bf2d925c56289ca4dada537433cc2640f332d
Ryan Wiemer <ryan@ryanwiemer.com>
2018-09-01 10:23:42 -0700

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
+import Title from '../components/Title'
+import { Link } from 'gatsby'
+
+export default () => {
+
+  return (
+    <>
+      <SEO/>
+      <Layout>
+          <Title>
+            404 - Page Not Found
+          </Title>
+          <Link to="/">Return Home</Link>
+      </Layout>
+    </>
+  )
+}
