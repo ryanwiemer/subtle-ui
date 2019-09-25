5e83d4559f552ac440c77335d0be77d180c5aff2
ryanwiemer <ryan@ryanwiemer.com>
2019-09-25 15:33:51 -0700

    Schema customization and data query changes

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { MDXProvider } from '@mdx-js/react'
 import Layout from '../components/Layout'
@@ -10,44 +9,31 @@ import BackButton from '../components/BackButton'
 import PostLinks from '../components/PostLinks'
 import { Markdown } from '../components/Markdown'
 import SEO from '../components/SEO'
-const PostTemplate = ({ data: { mdx }, pageContext, location }) => {
+const PostTemplate = ({ pageContext, location }) => {
+  const post = pageContext.post
+  const previous = pageContext.previous
+  const next = pageContext.next
   return (
     <>
-      <SEO title={mdx.frontmatter.title} url={mdx.fields.slug} />
-
+      <SEO title={post.frontmatter.title} url={post.fields.slug} />
       <Layout location={location}>
         <BackButton />
-        <Title>{mdx.frontmatter.title}</Title>
-        <PostLinks previous={pageContext.previous} next={pageContext.next} />
-        <Author name={mdx.frontmatter.author} github={mdx.frontmatter.github} />
+        <Title>{post.frontmatter.title}</Title>
+        <PostLinks previous={previous} next={next} />
+        <Author
+          name={post.frontmatter.author}
+          github={post.frontmatter.github}
+        />
         <MDXProvider
           components={{
             ...Markdown,
             code: CodeEditor,
           }}
         >
-          <MDXRenderer>{mdx.body}</MDXRenderer>
+          <MDXRenderer>{post.body}</MDXRenderer>
         </MDXProvider>
       </Layout>
     </>
   )
 }
-
-export const pageQuery = graphql`
-  query($slug: String!) {
-    mdx(fields: { slug: { eq: $slug } }) {
-      id
-      fields {
-        slug
-      }
-      body
-      frontmatter {
-        title
-        author
-        github
-        date(formatString: "MMMM DD, YYYY")
-      }
-    }
-  }
-`
 export default PostTemplate
